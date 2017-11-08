5b86fab9bd1a6c99585937db0f118e332688c39a
Jonathan de Jong <jonathandejong02@gmail.com>
2017-11-08 16:49:48 +0100

    personal fixes

--- cgoflags_windows.go
+++ cgoflags_windows.go
@@ -2,7 +2,7 @@
 
 package python
 
-// #cgo CFLAGS: -IC:/Python27/include
+// #cgo CFLAGS: -IC:/Python27/include -DMS_WIN64
 // #cgo LDFLAGS: -LC:/Python27/libs -lpython27
 // #include "go-python.h"
 import "C"
